32bdcda67a5b8624fa6bda99b0b96f3ac88313f2
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-07-29 17:51:18 +0300

    created theme

---
+++ lib/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    primary: Colors.grey.shade300,
+    surface: Colors.grey.shade200,
+  ),
+  scaffoldBackgroundColor: Colors.white,
+  cardColor:Colors.grey.shade400 ,
+  textTheme: TextTheme(
+    bodySmall: TextStyle(color: Colors.grey),
+    bodyMedium: TextStyle(color: Colors.black),
+    bodyLarge: TextStyle(color: Colors.grey.shade800),
+  ),
+);
+
+final ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  cardColor:Colors.grey,
+  colorScheme: ColorScheme.dark(
+    primary: Colors.grey.shade300,
+    surface: Colors.grey.shade800,
+  ),
+  scaffoldBackgroundColor: Colors.grey.shade900,
+
+  textTheme: TextTheme(
+    bodySmall: TextStyle(color: Colors.grey.shade100),
+    bodyMedium: TextStyle(color: Colors.white),
+    bodyLarge: TextStyle(color: Colors.grey.shade300),
+  ),
+);
