c5186a42b03e783f20e9e7c05b1eda4c5084aca3
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-08-05 18:25:07 +0300

    removed unused code

--- lib/providers/background_service.dart
+++ lib/providers/background_service.dart
@@ -1,10 +1,6 @@
 import 'dart:async';
 import 'package:flutter_background/flutter_background.dart';
-import 'package:pedometer/pedometer.dart';
 import 'package:permission_handler/permission_handler.dart';
-
-StreamSubscription<StepCount>? _stepCountStream;
-int _currentSteps = 0;
 
 Future<bool> initializeBackgroundService() async {
   final androidConfig = FlutterBackgroundAndroidConfig(
@@ -24,30 +20,4 @@ Future<bool> initializeBackgroundService() async {
   return permission;
 }
 
-Future<bool> startStepCounter() async {
-  bool started = await FlutterBackground.enableBackgroundExecution();
 
-  if (started) {
-    _stepCountStream = Pedometer.stepCountStream.listen(
-      _onStepCount,
-      onError: _onStepCountError,
-      cancelOnError: true,
-    );
-  }
-
-  return started;
-}
-
-Future<void> stopStepCounter() async {
-  await _stepCountStream?.cancel();
-  await FlutterBackground.disableBackgroundExecution();
-}
-
-void _onStepCount(StepCount event) {
-  _currentSteps = event.steps;
- 
-}
-
-void _onStepCountError(error) {
-  print('Step count error: $error');
-}
